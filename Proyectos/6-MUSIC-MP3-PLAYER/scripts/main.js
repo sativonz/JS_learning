1591575549a90fe04db240660bd2fd0afec66bc9
sativonz <fernandogarciafornes@gmail.com>
2023-03-07 21:10:02 +0100

    Refactor code

--- Proyectos/6-MUSIC-MP3-PLAYER/scripts/main.js
+++ Proyectos/6-MUSIC-MP3-PLAYER/scripts/main.js
@@ -1,233 +1,241 @@
 const d = document,
-    $controlsBox = d.getElementById("controls-box"),
-    $artBox = d.getElementById("art-box"),
-    $statusBox = d.getElementById("status-box"),
-    $artBoxIMG = d.querySelector(".art-box-img"),
-    $actualTime = d.querySelector(".actual-time"),
-    $totalTime = d.querySelector(".total-time"),
-    $play = d.querySelector(".controls .play"),
-    $pause = d.querySelector(".controls .pause"),
-    $next = d.querySelector(".controls .next"),
-    $previous = d.querySelector(".controls .previous"),
-    $equalizer = d.querySelector(".equalizer"),
-    $progressBar = d.querySelector(".progress-bar-mp3");
-
+    $ = (selector) => d.querySelector(selector),
+    $$ = (selector) => d.querySelectorAll(selector),
+    $controlsBox = $("#controls-box"),
+    $artBox = $("#art-box"),
+    $statusBox = $("#status-box"),
+    $artBoxIMG = $(".art-box-img"),
+    $actualTime = $(".actual-time"),
+    $totalTime = $(".total-time"),
+    $play = $(".controls .play"),
+    $pause = $(".controls .pause"),
+    $next = $(".controls .next"),
+    $previous = $(".controls .previous"),
+    $equalizer = $(".equalizer"),
+    $progressBar = $(".progress-bar-mp3");
 
 let nextSong = 0,
     progressBarCT,
-    setIntervalTime;
+    setIntervalTime,
+    actualAudio,
+    songsLength;
+
+const fetchSrpiteSVG = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "./assets/sprite.svg", true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            const hiddenSvg = d.createElement("svg");
+            hiddenSvg.style.display = "none";
+            hiddenSvg.innerHTML = xhr.responseText;
+            d.body.appendChild(hiddenSvg);
+        }
+    };
+    xhr.send();
+}
 
 const fetchSongs = async () => {
     try {
         const res = await fetch("./assets/db.json");
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        const data = await res.json();
-        return data;
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        return res.json();
     } catch (error) {
         console.log(error);
-        let message = error.statusText || "Ocurrió un error al recibir los datos";
+        const message = error.statusText || "Ocurrió un error al recibir los datos";
         $controlsBox.innerHTML = `<p>Error ${error.status}: ${message}</p>`;
     }
 };
 
 const loadSongs = async () => {
-    const data = await fetchSongs();
-    let { img, name, artist, url } = data.songs[nextSong];
+    const { songs } = await fetchSongs(),
+        { img, name, artist, url } = songs[nextSong];
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "./assets/sprite.svg", true);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const hiddenSvg = document.createElement("svg");
-            hiddenSvg.style.display = "none";
-            hiddenSvg.innerHTML = xhr.responseText;
-            document.body.appendChild(hiddenSvg);
-        }
-    };
-    xhr.send();
-
-    let actualAudio = new Audio(url);
-
-    const songsLength = data.songs.length;
+    actualAudio = new Audio(url);
+    songsLength = songs.length;
 
     $artBoxIMG.src = img;
-    $controlsBox.querySelector(".name-song").innerHTML = name;
-    $controlsBox.querySelector(".artist-song").innerHTML = artist;
+    $(".name-song").innerHTML = name;
+    $(".artist-song").innerHTML = artist;
 
-    let $ul = document.createElement("ul"),
-        $fragment = d.createDocumentFragment(),
-        playlistIndex = 0;
+    const playlist = songs
+        .map((song, index) => `<li data-id="${index}">${song.name}</li>`)
+        .join("");
 
-    $ul.classList.add("playlist"),
-    data.songs.forEach(song => {
-        const $li = document.createElement("li");
-        $li.textContent = song.name;
-        $li.dataset.id = playlistIndex;
-        $fragment.appendChild($li);
-        playlistIndex++;
-    });
-
-    $ul.appendChild($fragment);
+    const $ul = d.createElement("ul");
+    $ul.classList.add("playlist");
+    $ul.innerHTML = playlist;
     $artBox.appendChild($ul);
 
-    const getCurrentTime = () => {
-        const currentTime = actualAudio.currentTime,
-            totalSeconds = Math.floor(currentTime),
-            minutes = Math.floor(totalSeconds / 60),
-            seconds = totalSeconds % 60,
-            formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        $actualTime.innerHTML = formattedTime;
-    }
-    
-    const progressBarCurrentTime = () => {
-        if($progressBar.value == $progressBar.max){
-                clearInterval(progressBarCT);
-                $progressBar.setAttribute('max', 0);
-                $progressBar.setAttribute('value', 0);
-            if($next.classList.contains("last")){
+    const playNextSong = () => {
+        if (nextSong +1 != songsLength){
+            nextSong++;
+            nextSong+1  >= songsLength ? ($next.classList.add("last")) : $previous.classList.remove("last");
+            const { img, name, artist, url } = songs[nextSong];
+            $artBoxIMG.src = img;
+            $(".name-song").innerHTML = name;
+            $(".artist-song").innerHTML = artist;
+            actualAudio.pause();
+            actualAudio = new Audio(url);
+            togglePlayPause();
+            $progressBar.setAttribute("value", 0);
+            audioListeners();
+        } 
+
+    };
+
+    const playPreviousSong = () => {
+        if (nextSong > 0) {
+            nextSong--;
+            if (nextSong === 0) $previous.classList.add("last");
+            $next.classList.remove("last");
+            const { img, name, artist, url } = songs[nextSong];
+            $artBoxIMG.src = img;
+            $(".name-song").innerHTML = name;
+            $(".artist-song").innerHTML = artist;
+            actualAudio.pause();
+            actualAudio = new Audio(url);
+            togglePlayPause();
+            $progressBar.setAttribute("value", 0);
+            audioListeners();
+        } 
+    };
+
+    const audioListeners = () => {
+        actualAudio.addEventListener("loadedmetadata", () => {
+            const { duration } = actualAudio,
+                totalSeconds = Math.floor(duration),
+                minutes = Math.floor(totalSeconds / 60),
+                seconds = totalSeconds % 60,
+                formattedTime = `${String(minutes).padStart(2, "0")}: ${String(seconds).padStart(2, "0")}`;
+        
+            $totalTime.innerHTML = formattedTime;
+        });
+            
+        actualAudio.addEventListener("play", () => {
+            $play.classList.add("hide");
+            $pause.classList.remove("hide");
+            $artBoxIMG.classList.add("active");
+            $equalizer.classList.add("active");
+            $equalizer.classList.remove("hide");
+            $equalizer.classList.remove("pause");
+            progressBarCurrentTime();
+            getCurrentTime();
+            setIntervalTime = setInterval(() => {
+                getCurrentTime();
+                progressBarCurrentTime();
+            }, 1000);
+        });
+        
+        actualAudio.addEventListener("pause", () => {
+            $pause.classList.add("hide");
+            $play.classList.remove("hide");
+            $artBoxIMG.classList.remove("active");
+            $equalizer.classList.remove("active");
+            $equalizer.classList.add("pause");
+            clearInterval(progressBarCT);
+            clearInterval(setIntervalTime);
+        });
+        
+        actualAudio.addEventListener("ended", () => {
+            clearInterval(progressBarCT);
+            clearInterval(setIntervalTime);
+            if ($next.classList.contains("last")) {
                 $pause.classList.add("hide");
                 $play.classList.remove("hide");
+                $progressBar.setAttribute("max", 0);
+                $progressBar.setAttribute("value", 0);
             } else {
                 playNextSong();
             }
+        });
+    }
+
+    audioListeners();
+
+    d.addEventListener("click", e => {
+
+        if (e.target.matches(".playlist li")) {
             
-        } else {
-            $progressBar.setAttribute('max', actualAudio.duration);
-            $progressBar.setAttribute('value', actualAudio.currentTime);
-        }
-        
-    }
-
-    progressBarCT = setInterval(progressBarCurrentTime, 1000);
-
-    const getTotalTime = () => {
-        let totalMinutes = Math.floor(actualAudio.duration / 60),
-            totalSeconds = Math.floor(actualAudio.duration - totalMinutes * 60),
-            totalTime = totalMinutes + ':' + totalSeconds;
-        if (totalTime != NaN)  $totalTime.innerHTML = totalTime;
-      
-    }
-
-    async function playMusic() { 
-        $totalTime.innerHTML = "";
-        clearInterval(setIntervalTime);
-        if ($equalizer.classList.contains("pause")) $equalizer.classList.remove("pause"); 
-        setIntervalTime = setInterval(getCurrentTime, 500);
-        await actualAudio.play();
-        $actualTime.classList.remove("hide");
-        $play.classList.add("hide");
-        $equalizer.classList.remove("hide");
-        $pause.classList.remove("hide");
-        $actualTime.classList.remove("hide");
-        getTotalTime();
-    }
-
-    const playNextSong = () => {
-        if ($previous.classList.contains('last')) $previous.classList.remove('last');
-            if (nextSong + 1 < songsLength) {
-                $actualTime.innerHTML = "";
-                $totalTime.innerHTML = "";
-                $actualTime.classList.add("hide")
-                $equalizer.classList.add("pause");
-                clearInterval(setIntervalTime);
-                clearInterval(progressBarCT);
-                actualAudio.pause();
-                actualAudio.currentTime = 0;
-                nextSong++;
-                actualAudio = new Audio(data.songs[nextSong].url);
-                actualAudio.addEventListener("canplaythrough", () => { if ($play.classList.contains('hide')){
-                    playMusic();
-                    $actualTime.classList.remove("hide")
-                }}) 
-                $artBoxIMG.src = data.songs[nextSong].img;
-                $controlsBox.querySelector(".name-song").innerHTML = data.songs[nextSong].name;
-                $controlsBox.querySelector(".artist-song").innerHTML = data.songs[nextSong].artist;
-                if (nextSong == songsLength - 1) $next.classList.add("last");
-                progressBarCT = setInterval(progressBarCurrentTime, 1000);
+            nextSong = Number(e.target.dataset.id);
+            if (nextSong === songsLength - 1) {
+                $next.classList.add("last");
+                $previous.classList.remove("last");
+            } else if (nextSong === 0) {
+                $previous.classList.add("last");
+                $next.classList.remove("last");
+            } else {
+                $previous.classList.remove("last");
+                $next.classList.remove("last");
             }
-    }
-
-
-    d.addEventListener("click", (e) => {
-
-        if (e.target.matches("progress.progress-bar-mp3")) {
-            if (actualAudio.currentTime > 0) {
-                clearInterval(progressBarCT);
-                const x = e.pageX - $progressBar.offsetLeft;
-                let clickedValue = x * $progressBar.max / $progressBar.offsetWidth;
-                clickedValue = clickedValue.toFixed(0);
-                $progressBar.value = clickedValue;
-                actualAudio.currentTime = clickedValue;
-                progressBarCT = setInterval(progressBarCurrentTime, 1000);
-                getCurrentTime();
-            }
-        }
-
-        if (e.target.matches(".play")) playMusic();
-
-        if (e.target.matches(".pause")) {
-            $equalizer.classList.add("pause");
+                        
+            const { img, name, artist, url } = songs[nextSong];
+            $artBoxIMG.src = img;
+            $(".name-song").innerHTML = name;
+            $(".artist-song").innerHTML = artist;
             actualAudio.pause();
-            $pause.classList.add("hide");
-            $play.classList.remove("hide");
-            clearInterval(setIntervalTime)
-        }
-
-        if (e.target.matches(".next")) playNextSong();
-
-        if (e.target.matches(".previous")) {
-            if (nextSong > 0) {
-                $actualTime.innerHTML = "";
-                $totalTime.innerHTML = "";
-                $actualTime.classList.add("hide")
-                $equalizer.classList.add("pause");
-                if ($next.classList.contains('last')) $next.classList.remove('last');
-                clearInterval(setIntervalTime);
-                actualAudio.pause();
-                actualAudio.currentTime = 0;
-                nextSong--;
-                actualAudio = new Audio(data.songs[nextSong].url);
-                actualAudio.addEventListener("canplaythrough", () => { if ($play.classList.contains('hide')) {
-                    playMusic();
-                    $actualTime.classList.remove("hide")
-                }});
-                $artBoxIMG.src = data.songs[nextSong].img;
-                $controlsBox.querySelector(".name-song").innerHTML = data.songs[nextSong].name;
-                $controlsBox.querySelector(".artist-song").innerHTML = data.songs[nextSong].artist;
-                if (nextSong == 0) $previous.classList.add("last");
-            }
-        }
-
-        if(e.target.matches(".playlist_add") || e.target.matches(".playlist_add use") ){
-
-            
-            let $playlist = d.querySelector(".playlist"),
-                $playlistDisplay =  getComputedStyle($playlist).getPropertyValue("visibility");
-        
-                if ($playlistDisplay === 'hidden') $playlist.classList.add("open");
-                else $playlist.classList.remove("open");     
-        }
-       
-        if(e.target.matches(".playlist li")){
-            nextSong = parseInt(e.target.dataset.id);
-            if (nextSong == 0) $previous.classList.add("last"), $next.classList.remove("last");
-            else if  (nextSong == songsLength - 1) $next.classList.add("last"),$previous.classList.remove("last")
-            else if (nextSong != songsLength - 1 || nextSong != 0) $next.classList.remove("last"),$previous.classList.remove("last");
-            clearInterval(setIntervalTime);
-            clearInterval(progressBarCT);
-            actualAudio.pause();
-            actualAudio.currentTime = 0;
-            actualAudio = new Audio(data.songs[e.target.dataset.id].url);
-            playMusic();
-            $artBoxIMG.src = data.songs[e.target.dataset.id].img;
-            $controlsBox.querySelector(".name-song").innerHTML = data.songs[e.target.dataset.id].name;
-            $controlsBox.querySelector(".artist-song").innerHTML = data.songs[e.target.dataset.id].artist;
-            progressBarCT = setInterval(progressBarCurrentTime, 1000);
-        }
+            actualAudio = new Audio(url);
+            togglePlayPause();
+            $play.classList.add("hide");
+            $pause.classList.remove("hide");
+            $equalizer.classList.add("active");
+            $equalizer.classList.remove("hide");
+            setInterval(() => progressBarCurrentTime(), 1000);
+            audioListeners();
+        };
 
     })
-}
 
-d.addEventListener("DOMContentLoaded", e => loadSongs());
+    $play.addEventListener("click", togglePlayPause);
+    $pause.addEventListener("click", togglePlayPause);
+    $next.addEventListener("click", playNextSong);
+    $previous.addEventListener("click", playPreviousSong);
+    
+};
+
+const getCurrentTime = () => {
+    const { currentTime } = actualAudio,
+        totalSeconds = Math.floor(currentTime),
+        minutes = Math.floor(totalSeconds / 60),
+        seconds = totalSeconds % 60,
+        formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+    $actualTime.innerHTML = formattedTime;
+};
+
+const progressBarCurrentTime = () => {
+    if ($progressBar.value === $progressBar.max) {
+        clearInterval(progressBarCT);
+        $progressBar.setAttribute("max", 0);
+        $progressBar.setAttribute("value", 0);
+        $next.classList.contains("last") ? ($pause.classList.add("hide"), $play.classList.remove("hide")) : playNextSong();
+    } else {
+        $progressBar.setAttribute("max", actualAudio.duration);
+        $progressBar.setAttribute("value", actualAudio.currentTime);
+    }
+};
+
+const togglePlayPause = () => actualAudio.paused ? actualAudio.play() : actualAudio.pause();
+
+d.addEventListener("click", e => {
+
+    if (e.target.matches("progress.progress-bar-mp3")) {
+        if (actualAudio.currentTime > 0) {
+            clearInterval(progressBarCT);
+            const x = e.pageX - $progressBar.offsetLeft;
+            let clickedValue = x * $progressBar.max / $progressBar.offsetWidth;
+            clickedValue = clickedValue.toFixed(0);
+            $progressBar.value = clickedValue;
+            actualAudio.currentTime = clickedValue;
+            getCurrentTime();
+        }
+    }
+
+    if(e.target.matches(".playlist_add") || e.target.matches(".playlist_add use") ){
+        let $playlist = d.querySelector(".playlist"),
+            $playlistDisplay =  getComputedStyle($playlist).getPropertyValue("visibility");
+        $playlistDisplay === 'hidden' ? $playlist.classList.add("open") : $playlist.classList.remove("open");     
+    }
+
+})
+
+d.addEventListener("DOMContentLoaded", e => loadSongs(), fetchSrpiteSVG());
